27760673af11eabbd28960a3ad26b0315fb4d9b8
Teennss <tenster0620@gmail.com>
2023-04-25 13:19:24 +0900

    test

---
+++ JS/script.js
@@ -0,0 +1,37 @@
+const btnNavEl = document.querySelector(".navphonebut");
+const headerEl = document.querySelector(".yokobar");
+const btnNavcloseEl = document.querySelector(".yokobarbut");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+btnNavcloseEl.addEventListener("click", function () {
+    headerEl.classList.toggle("nav-open");
+  });
+
+  const allLinks = document.querySelectorAll("a:link");
+
+  allLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const href = link.getAttribute("href");
+  
+      // Scroll back to top
+      if (href === "#")
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+  
+      // Scroll to other links
+      if (href !== "#" && href.startsWith("#")) {
+        const sectionEl = document.querySelector(href);
+        sectionEl.scrollIntoView({ behavior: "smooth" });
+      }
+  
+      // Close mobile naviagtion
+      if (link.classList.contains("nav-list-button"))
+        headerEl.classList.toggle("nav-open");
+    });
+  });
