fdb7440c469686a918de1dc356af9d8deb0fcb2a
Teennss <tenster0620@gmail.com>
2023-04-26 17:00:37 +0900

    test

--- JS/script.js
+++ JS/script.js
@@ -36,12 +36,78 @@ btnNavcloseEl.addEventListener("click", function () {
     });
   });
 
+  
+
+
+  // topbut
+
+
+  var backButton = document.querySelector('.topbut');
+
+  window.addEventListener('scroll', function() {
+
+    var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    
+
+    if (scrollPosition > window.innerHeight) {
+      backButton.classList.add('show');
+    } else {
+      backButton.classList.remove('show');
+    }
+  });
+  
+
+
+
+
+
 
   // scrollbar 
 
-  var element = document.querySelector('.shopxbox');
-  var scrollWidth = element.scrollWidth;
-  var clientWidth = element.clientWidth;
-  var scrollLeft = (scrollWidth - clientWidth) / 2;
-  element.scrollLeft = scrollLeft;
+  window.onload = function() {
+    var elements = document.querySelectorAll('.shopxbox');
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var scrollWidth = element.scrollWidth;
+      var clientWidth = element.clientWidth;
+      var scrollLeft = (scrollWidth - clientWidth) / 2;
+      element.scrollLeft = scrollLeft;
+    }
+    
+  };
   
+  
+
+  //cvbox
+
+  const cvbtnNavEl = document.querySelectorAll(".cvbut");
+  const cvheaderEl = document.querySelectorAll(".cvtext1");
+  
+  cvbtnNavEl.forEach(function (btn, index) {
+    btn.addEventListener("click", function () {
+      cvheaderEl[index].classList.toggle("cvtextopen");
+    });
+  });
+  
+  // qabox
+
+  const qabtnNavEl = document.querySelectorAll(".qabut");
+  const qaheaderEl = document.querySelectorAll(".qatext1");
+  
+  qabtnNavEl.forEach(function (btn, index) {
+    btn.addEventListener("click", function () {
+      qaheaderEl[index].classList.toggle("qatextopen");
+    });
+  });
+  
+// moreshopinfo
+
+
+  const morebarbtnNavEl = document.querySelectorAll(".moreshopbar");
+  const morebarheaderEl = document.querySelectorAll(".moreshopinfo");
+
+  morebarbtnNavEl.forEach(function (btn, index) {
+    btn.addEventListener("click", function () {
+      morebarheaderEl[index].classList.toggle("moreshopinfo-open");
+    });
+  });
